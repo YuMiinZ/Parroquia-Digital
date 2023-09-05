963d7701d5850787c91eaa1504f7384107ce884d
YuMiinZ <yumiin00@hotmail.com>
2023-09-05 17:56:15 -0600

    Primer commit

---
+++ Parroquia Digital/Parroquia Digital/Pages/Grupos.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Parroquia_Digital.Pages
+{
+    public class GruposModel : PageModel
+    {
+        public string CustomTitle { get; set; }
+
+        public void OnGet()
+        {
+            CustomTitle = "Grupos";
+        }
+    }
+}
---
+++ Parroquia Digital/Parroquia Digital/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parroquia_Digital.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string CustomTitle { get; set; }
+
+        public void OnGet()
+        {
+            CustomTitle = "Parroquia de San Miguel de Desamparados";
+        }
+    }
+    
+}
