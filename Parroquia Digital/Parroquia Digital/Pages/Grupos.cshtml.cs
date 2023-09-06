7ec502b592855ead0f215172c983e7744c3bda1a
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2023-09-05 21:50:01 -0600

    Conexión con Sheets

--- Parroquia Digital/Parroquia Digital/Pages/Grupos.cshtml.cs
+++ Parroquia Digital/Parroquia Digital/Pages/Grupos.cshtml.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+using Google.Apis.Services;
 
 namespace Parroquia_Digital.Pages
 {
@@ -11,9 +15,57 @@ namespace Parroquia_Digital.Pages
     {
         public string CustomTitle { get; set; }
 
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string SpreadsheetId = "1lsOwRmj1-NcnuFh8j0naaoiWDd1r_NTg6CX_fBbgoGQ";
+        static readonly string sheet = "Grupos";
+        static SheetsService service;
+
+
         public void OnGet()
         {
             CustomTitle = "Grupos";
+
+            CreateCredential();
+
+            ReadEntries();
+        }
+
+        static void CreateCredential()
+        {
+            var applicationName = "Grupos";
+
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+            }
+
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName,
+            });
+        }
+
+        static void ReadEntries()
+        {
+            var range = $"{sheet}!A2:C5";
+            var request = service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+            var response = request.Execute();
+            var values = response.Values;
+
+            if(values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    Console.WriteLine("{0} | {1} | {2}", row[0], row[1], row[2]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No hay datos");
+            }
         }
     }
 }
